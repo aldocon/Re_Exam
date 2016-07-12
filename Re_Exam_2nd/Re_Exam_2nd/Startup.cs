ce18036ca9bbc576ad0667198353c9c09adf6aec
aldocon <aldocon38@gmail.com>
2016-07-12 10:21:46 +0200

    big ass update

---
+++ Re_Exam_2nd/Re_Exam_2nd/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Re_Exam_2nd.Startup))]
+namespace Re_Exam_2nd
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
